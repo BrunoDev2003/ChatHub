aa3cfc1facb0c069c357e1c325a3a6f8489a050d
Bruno Gusmão <brunogusmao43@gmail.com>
2024-03-13 15:51:08 -0300

    Implementando a classe de mensagem e a Room.

---
+++ src/main/java/com/chathub/chathub/model/Room.java
@@ -0,0 +1,22 @@
+package com.chathub.chathub.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Room {
+    private String id;
+    private String[] names;
+
+    public Room(String id, String name) {
+        this.id = id;
+        this.names[0] = name;
+    }
+
+    public Room(String id, String name1, String name2) {
+        this.id = id;
+        this.names[0] = name1;
+        this.names[1] = name2;
+    }
+}
