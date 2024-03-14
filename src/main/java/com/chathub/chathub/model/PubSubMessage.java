66d8d24e3944cb4afa46e901fbde2091cf8e64d5
Bruno Gusmão <brunogusmao43@gmail.com>
2024-03-14 16:46:01 -0300

    Implementando a classe de PubSub e de LoginData

---
+++ src/main/java/com/chathub/chathub/model/PubSubMessage.java
@@ -0,0 +1,16 @@
+package com.chathub.chathub.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PubSubMessage<T> {
+    private String type;
+    private T data;
+
+    public PubSubMessage(String type, T data) {
+        this.type = type;
+        this.data = data;
+    }
+}
