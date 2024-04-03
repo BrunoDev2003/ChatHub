8d9953840b37def1e3911f4771d3e005c34e821d
Bruno Gusmão <brunogusmao43@gmail.com>
2024-04-03 18:24:59 -0300

    .

--- src/main/java/com/chathub/chathub/config/RedisAppConfig.java
+++ src/main/java/com/chathub/chathub/config/RedisAppConfig.java
@@ -9,7 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
@@ -32,9 +35,47 @@ public class RedisAppConfig {
 
     @Bean
     RedisMessageListenerContainer redisContainer(RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter messageListener)
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
+    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory);
-        container.addMessageListener(messageListener(), topic());
+        container.addMessageListener(
+
+    messageListener(),topic());
         return container;
+
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("MESSAGES");
     }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        //ler variaveis de ambiente
+        String endpointUrl = System.getenv("REDIS_ENDPOINT_URL");
+        if (endpointUrl == null) {
+            endpointUrl = "127.0.0.1:6379";
+        }
+
+        String password = System.getenv("REDIS_PASSWORD");
+
+        String[] urlParts = endpointUrl.split(":");
+
+        String host = urlParts[0];
+        String port = "0";
+
+        if (urlParts.length > 1) {
+            port = urlParts[1];
+        }
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, Integer.parseInt(port));
+
+        System.out.printf("Conectando ao %s:%s com a senha: %s%n", host, port, password);
+
+        if (password != null) {
+            redisStandaloneConfiguration.setPassword(password);
+        }
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
 }
