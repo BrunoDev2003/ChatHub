154b1fa6717b281b5255a4502cb3d4536fc8a7d5
Bruno Gusmão <brunogusmao43@gmail.com>
2024-05-14 17:06:29 -0300

    arrumando bugs de dependencias e de imports.

--- src/main/java/com/chathub/chathub/model/Room.java
+++ src/main/java/com/chathub/chathub/model/Room.java
@@ -2,9 +2,11 @@ package com.chathub.chathub.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 public class Room {
     private String id;
     private String[] names;
