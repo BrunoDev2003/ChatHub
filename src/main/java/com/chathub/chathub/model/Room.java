a679a6bce1daac3748415c9123c20acee64e4aea
Bruno Gusmão <brunogusmao43@gmail.com>
2024-05-22 18:05:21 -0300

    Escrevendo a lógica do controller do chat.

--- src/main/java/com/chathub/chathub/model/Room.java
+++ src/main/java/com/chathub/chathub/model/Room.java
@@ -11,13 +11,19 @@ public class Room {
     private String id;
     private String[] names;
 
+    public Room() {
+        this.names = new String[2];
+    }
+
     public Room(String id, String name) {
         this.id = id;
+        this.names = new String[1];
         this.names[0] = name;
     }
 
     public Room(String id, String name1, String name2) {
         this.id = id;
+        this.names = new String[2];
         this.names[0] = name1;
         this.names[1] = name2;
     }
--- src/main/java/com/chathub/chathub/service/MessageSubscriber.java
+++ src/main/java/com/chathub/chathub/service/MessageSubscriber.java
@@ -2,16 +2,35 @@ package com.chathub.chathub.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cglib.core.internal.Function;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class MessageSubscriber implements MessageListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageSubscriber.class);
 
+    CopyOnWriteArrayList<Function<String, Integer>> handlers = new CopyOnWriteArrayList<>();
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
+        String messageBody = new String(message.getBody());
 
+        LOGGER.info("Mensagem recebida no global subscriber: " + message.toString());
+
+        for (Function<String, Integer> handler : handlers) {
+            handler.apply(messageBody);
+        }
+    }
+
+    public void attach(Function<String, Integer> handler) {
+        handlers.add(handler);
+    }
+
+    public void detach(Function<String, Integer> handler) {
+        handlers.remove(handler);
     }
 }
